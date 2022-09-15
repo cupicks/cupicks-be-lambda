8e645bbb640734024b13ab6008da015cdf43417e
axis <axisotherwise@gmail.com>
2022-09-16 01:21:18 +0900

    Update comment resizing

--- profile-resizing/index.js
+++ profile-resizing/index.js
@@ -1,9 +1,9 @@
-import sharp from "sharp";
-import AWS, { AWSError } from "aws-sdk";
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
 
 const s3 = new AWS.S3();
 
-export const handler = async (event, context, done) => {
+exports.handler = async (event, context, done) => {
     const Bucket = event.Records[0].s3.bucket.name;
     const Key = event.Records[0].s3.object.key;
 
@@ -13,25 +13,24 @@ export const handler = async (event, context, done) => {
 
     try {
         // 이미지 버퍼
-        const s3Object: AWSError | any = await s3.getObject({ Bucket, Key }).promise();
+        const s3Object = await s3.getObject({ Bucket, Key }).promise();
 
         // 리사이징 옵션
         const resizedImage = await sharp(s3Object.Body)
         .resize(400, 400, { fit: "inside" })
         .toFormat(format)
         .toBuffer();
-
         await s3.putObject(
             {
                 Bucket,
-                Key: `profile-resized/${filename}`, // 저장 경로
+                Key: `comment-resized/${filename}`, // 저장 경로
                 Body: resizedImage,
             }
         ).promise();
 
-        return done(null, `profile-resized/${filename}`);
+        return done(null, `comment-resized/${filename}`);
     } catch (error) {
         console.error(error);
         return done(error);
     }
-}
+};
