c5dca4e5c173f5277ea432acc34e109c751c29f4
axis <axisotherwise@gmail.com>
2022-09-16 18:21:57 +0900

    Update profile image resizing

--- profile-resizing/index.js
+++ profile-resizing/index.js
@@ -1,21 +1,23 @@
-import sharp from "sharp";
-import AWS from "aws-sdk";
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
 
 const s3 = new AWS.S3();
 
-export const handler = async (event, context, done) => {
+exports.handler = async (event, context, done) => {
     const Bucket = event.Records[0].s3.bucket.name;
     const Key = event.Records[0].s3.object.key;
 
-    const filename = Key.split("/")[Key.split("/").length - 1];
-    const ext = Key.split(".")[Key.split('.').length - 1];
-    const format = ext === "jpg" ? "jpeg" : ext;
+    const filename = Key.split('/')[Key.split('/').length - 1];
+    const ext = Key.split('.')[Key.split('.').length - 1];
+    const format = ext === 'jpg' ? 'jpeg' : ext;
 
     try {
-        const s3Object: any = await s3.getObject({ Bucket, Key }).promise();
+        // 이미지 버퍼 형식
+        const s3Object = await s3.getObject({ Bucket, Key }).promise();
 
+        // 리사이징 옵션
         const resizedImage = await sharp(s3Object.Body)
-        .resize(200, 200, { fit: "inside" })
+        .resize(200, 200, { fit: 'inside' })
         .toFormat(format)
         .toBuffer();
 
@@ -24,11 +26,10 @@ export const handler = async (event, context, done) => {
         Key: `profile-resized/${filename}`,
         Body: resizedImage,
         }).promise();
-        
+
         return done(null, `profile-resized/${filename}`);
     } catch (error) {
         console.error(error);
         return done(error);
     }
-}
-
+};
