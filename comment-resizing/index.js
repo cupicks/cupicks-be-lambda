3131a4094a2f808a99fd3aa5a331d55e6ce12733
axis <axisotherwise@gmail.com>
2022-10-02 05:09:16 +0900

    Update comment

---
+++ comment-resizing/index.js
@@ -0,0 +1,46 @@
+const AWS = require("aws-sdk");
+const sharp = require("sharp");
+
+const s3 = new AWS.S3();
+
+exports.handler = async (event, context, done) => {
+    const Bucket = event.Records[0].s3.bucket.name;
+    const Key = event.Records[0].s3.object.key;
+    const filename = Key.split("/")[Key.split("/").length - 1];
+
+    const ext = Key.split(".")[Key.split(".").length - 1];
+    const format = ext === "jpg" ? "jpeg" : ext;
+
+    try {
+        const s3Object = await s3.getObject({ Bucket, Key }).promise();
+
+        const { width } = await sharp(s3Object.Body).metadata();
+
+        if (width < 400) {
+            await s3.putObject(
+                {
+                    Bucket,
+                    Key: `comment-resized/${filename}`,
+                    Body: resizedImage,
+                }
+                ).promise();
+        } else {
+            const resizedImage = await sharp(s3Object.Body)
+            .resize(400, 400, { fit: "inside" })
+            .toFormat(format)
+            .toBuffer();
+
+            await s3.putObject(
+                {
+                    Bucket,
+                    Key: `comment-resized/${filename}`,
+                    Body: resizedImage,
+                }
+                ).promise();
+        }
+
+        return done(null, `comment-resized/${filename}`);
+    } catch (error) {
+        return done(error);
+    }
+};
