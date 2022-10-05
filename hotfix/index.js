eb6ad5f3444d5d3a1027824817a33b8da7e2d21c
axis <axisotherwise@gmail.com>
2022-10-06 05:23:53 +0900

    hotfix

---
+++ hotfix/index.js
@@ -0,0 +1,73 @@
+const AWS = require("aws-sdk");
+const sharp = require("sharp");
+
+const s3 = new AWS.S3();
+
+exports.handler = async (event, context, done) => {
+    const Bucket = event.Records[0].s3.bucket.name;
+    const Key = event.Records[0].s3.object.key;
+    const filename = Key.split("/")[Key.split("/").length - 1];
+
+    const ext = Key.split(".")[Key.split(".").length - 1];
+    const format = ext === "jpg" ? "jpeg" : ext;
+
+    try {
+        const s3Object = await s3.getObject({ Bucket, Key }).promise();
+
+        const { width } = await sharp(s3Object.Body).metadata();
+
+        if (format === "png") { // PNG
+            if (width < 150) {
+                return await s3.putObject(
+                    {
+                        Bucket,
+                        Key: `lee-resized/${filename}`,
+                        Body: s3Object.Body,
+                    }
+                ).promise();
+            } else {
+                const pngResized = await sharp(s3Object.Body)
+                .resize(150, 150, { fit: "inside"})
+                .png({ palette: true })
+                .toBuffer();
+
+                return await s3.putObject(
+                    {
+                        Bucket,
+                        Key: `lee-resized/${filename}`,
+                        Body: pngResized,
+                    }
+                ).promise();
+            }
+        }
+
+        if (width < 150) { // JPEG
+            return await s3.putObject(
+                {
+                    Bucket,
+                    Key: `lee-resized/${filename}`,
+                    Body: s3Object.Body,
+                }
+                ).promise();
+        } else {
+            const jpegResized = await sharp(s3Object.Body)
+            .resize(150, 150, { fit: "inside" })
+            .toFormat(format)
+            .toBuffer();
+
+            return await s3.putObject(
+                {
+                    Bucket,
+                    Key: `lee-resized/${filename}`,
+                    Body: jpegResized,
+                }
+                ).promise();
+        }
+
+    } catch (err) {
+        console.log(err);
+        return done(err);
+    } finally {
+        return done(null, `lee-resized/${filename}`);
+    }
+};
