748d651794996a1f74e2fafdf94f2861c0329652
axis <axisotherwise@gmail.com>
2022-10-06 17:11:57 +0900

    Update hotfix PNG 리사이징 로직 수정

--- hotfix/index.js
+++ hotfix/index.js
@@ -4,70 +4,69 @@ const sharp = require("sharp");
 const s3 = new AWS.S3();
 
 exports.handler = async (event, context, done) => {
-    const Bucket = event.Records[0].s3.bucket.name;
-    const Key = event.Records[0].s3.object.key;
-    const filename = Key.split("/")[Key.split("/").length - 1];
+    console.log("here");
+    // const Bucket = event.Records[0].s3.bucket.name;
+    // const Key = event.Records[0].s3.object.key;
+    // const filename = Key.split("/")[Key.split("/").length - 1];
 
-    const ext = Key.split(".")[Key.split(".").length - 1];
-    const format = ext === "jpg" ? "jpeg" : ext;
+    // const ext = Key.split(".")[Key.split(".").length - 1];
+    // const format = ext === "jpg" ? "jpeg" : ext;
 
-    try {
-        const s3Object = await s3.getObject({ Bucket, Key }).promise();
+    // try {
+    //     const s3Object = await s3.getObject({ Bucket, Key }).promise();
 
-        const { width } = await sharp(s3Object.Body).metadata();
+    //     const { width } = await sharp(s3Object.Body).metadata();
 
-        if (format === "png") { // PNG
-            if (width < 150) {
-                return await s3.putObject(
-                    {
-                        Bucket,
-                        Key: `lee-resized/${filename}`,
-                        Body: s3Object.Body,
-                    }
-                ).promise();
-            } else {
-                const pngResized = await sharp(s3Object.Body)
-                .resize(150, 150, { fit: "inside"})
-                .png({ palette: true })
-                .toBuffer();
+    //     if (format === "png") { // PNG
+    //         if (width < 150) {
+    //             return await s3.putObject(
+    //                 {
+    //                     Bucket,
+    //                     Key: `lee-resized/${filename}`,
+    //                     Body: s3Object.Body,
+    //                 }
+    //             ).promise();
+    //         } else {
+    //             const pngResized = await sharp(s3Object.Body)
+    //             .resize(150, 150, { fit: "inside"})
+    //             .png({ palette: true })
+    //             .toBuffer();
 
-                return await s3.putObject(
-                    {
-                        Bucket,
-                        Key: `lee-resized/${filename}`,
-                        Body: pngResized,
-                    }
-                ).promise();
-            }
-        }
+    //             return await s3.putObject(
+    //                 {
+    //                     Bucket,
+    //                     Key: `lee-resized/${filename}`,
+    //                     Body: pngResized,
+    //                 }
+    //             ).promise();
+    //         }
+    //     }
 
-        if (width < 150) { // JPEG
-            return await s3.putObject(
-                {
-                    Bucket,
-                    Key: `lee-resized/${filename}`,
-                    Body: s3Object.Body,
-                }
-                ).promise();
-        } else {
-            const jpegResized = await sharp(s3Object.Body)
-            .resize(150, 150, { fit: "inside" })
-            .toFormat(format)
-            .toBuffer();
+    //     if (width < 150) { // JPEG
+    //         return await s3.putObject(
+    //             {
+    //                 Bucket,
+    //                 Key: `lee-resized/${filename}`,
+    //                 Body: s3Object.Body,
+    //             }
+    //             ).promise();
+    //     } else {
+    //         const jpegResized = await sharp(s3Object.Body)
+    //         .resize(150, 150, { fit: "inside" })
+    //         .toFormat(format)
+    //         .toBuffer();
 
-            return await s3.putObject(
-                {
-                    Bucket,
-                    Key: `lee-resized/${filename}`,
-                    Body: jpegResized,
-                }
-                ).promise();
-        }
+    //         return await s3.putObject(
+    //             {
+    //                 Bucket,
+    //                 Key: `lee-resized/${filename}`,
+    //                 Body: jpegResized,
+    //             }
+    //             ).promise();
+    //     }
 
-    } catch (err) {
-        console.log(err);
-        return done(err);
-    } finally {
-        return done(null, `lee-resized/${filename}`);
-    }
+    // } catch (err) {
+    //     // console.log(err);
+    //     return done(err);
+    // }
 };
